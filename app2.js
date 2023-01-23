76e737097bf80ba2c303d12b965f6c2af2a8b78f
tomasAybar <aybar.t4@gmail.com>
2023-01-23 13:07:53 -0300

    armado de la estructura del proyecto

--- app2.js
+++ app2.js
@@ -2,17 +2,17 @@
 let clientes = [];
 
 let habitaciones = [
-    {   
+    {
         opcion: 'A',
         tipo: 'standar',
         precioNoche: 2000
     },
-    {   
+    {
         opcion: 'B',
         tipo: 'full',
         precioNoche: 2500
     },
-    {   
+    {
         opcion: 'C',
         tipo: 'premium',
         precioNoche: 3000
@@ -35,30 +35,103 @@ class Cliente {
 
 /** FUNCIONES */
 
-const agregarCliente = (nombre, edad, habitacion, cantidadNoches) => {
-    clientes.push( new Cliente(nombre, edad, habitacion, cantidadNoches) );
+/**
+ * 
+ * @param {string} nombre 
+ * @param {number} edad 
+ * @param {string} habitacion 
+ * @param {number} cantidadNoches 
+ */
+const agregarCliente = () => {
+
+    do {
+
+        const nombre = prompt('Ingrese nombre del cliente');
+        const edad = parseInt(prompt('Ingrese edad del cliente'));
+        const habitacion = prompt('Ingrese el tipo de habitacion del cliente');
+        const cantidadNoches = parseInt(prompt('Ingrese la cantidad de noches a hospedarse del cliente'));
+
+        clientes.push(new Cliente(nombre, edad, habitacion, cantidadNoches));
+
+    } while ( confirm('Desea agregar otro cliente?') )
+
+
+
 }
 
-const mostrarHabitaciones = () => {
+/**
+ * 
+ * @param { array } arrayClientes
+ */
+const verClientes = (arrayClientes) => {
 
-    const infoHabitaciones = habitaciones.map(habitacion => `${ habitacion.opcion }) tipo: ${ habitacion.tipo.toUpperCase() } - precio por noche: $${ habitacion.precioNoche } \n` );
+    for (const cliente of arrayClientes) {
 
-    return infoHabitaciones;
+        console.log(cliente.nombre);
+        console.log(cliente.edad);
+        console.log(cliente.habitacion);
+        console.log(cliente.cantidadNoches);
+
+    }
+
+
 
 }
 
 
-const confirmRegistro = confirm(`Bienvenido al hotel codeFull\nA continuacion se le mostraran nuestras habitaciones disponibles y precios\nSi esta de acuerdo precione "OK" para continuar con el registro\n\n ${ mostrarHabitaciones() }`);
+const nombreAdministrador = prompt('Bienvenido administrador, ingrese su nombre para comenzar a trabajar');
 
-if ( confirmRegistro ) {
+console.log(nombreAdministrador);
 
-    console.log('Bienvenido');
+alert(`Hola ${nombreAdministrador.toUpperCase()}, A continuacion se le mostrara una lista con las acciones disponibles para hacer`);
 
+// Menu principal
+let accionIngresada = prompt(`Ingrese la opcion de la accion que desea realizar\n A) Ver todos los clientes ingresados\n B) Agregar nuevo cliente\n C) Filtrar Cliente\n D) Eliminar un cliente`).toUpperCase();
 
-    
-
-} else {
-
-    alert('Gracias por su visita');
+while (!['A', 'B', 'C', 'D'].includes(accionIngresada)) {
+    accionIngresada = prompt(`${'Opcion Incorrecta!!'.toUpperCase()}\nIngrese la opcion de la accion que desea realizar\n A) Ver todos los clientes ingresados\n B) Agregar nuevo cliente\n C) Filtrar Cliente\n D) Eliminar un cliente`).toUpperCase();
 }
+
+console.log(accionIngresada);
+
+// Ingreso a la opcion elegida
+switch (accionIngresada) {
+
+    case 'A':
+        console.log('La opcion elegida es la A');
+
+        // LLAMAR A FUNCION VER TODOS LOS CLIENTES INGRESADOS
+
+        verClientes(clientes);
+
+        break;
+
+    case 'B':
+        console.log('La opcion elegida es la B');
+
+        // LLAMAR A LA FUNCION AGREGAR NUEVO CLIENTE
+
+        agregarCliente();
+
+
+        break;
+
+    case 'C':
+        console.log('La opcion elegida es la C');
+
+        // LLAMAR A FUNCION FILTRAR CLIENTE
+        break;
+
+    case 'D':
+        console.log('La opcion elegida es la D');
+
+        // LLAMAR A FUNCION ELIMINAR UN CLIENTE
+        break;
+
+    default:
+        break;
+
+}
+
+
 
