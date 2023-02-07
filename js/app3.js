5bed499121bfbe666ad3f3620b8badbabdce2264
tomasAybar <aybar.t4@gmail.com>
2023-02-07 15:10:12 -0300

    implementacion de localstorage

--- js/app3.js
+++ js/app3.js
@@ -1,3 +1,4 @@
+// data
 const habitaciones = [
     {
         id: 1,
@@ -49,72 +50,232 @@ const habitaciones = [
         descripcion: 'Nuestro apart cuenta con la comodidad ideal para tres personas. Se trata de un departamento de un ambiente. Posee tv por cable y ventilador, Podrá acceder al primer piso por escalera.',
     },
 ]
-let carrito = [];
 
-const inputIndex = document.querySelector('#input-index');
-// const btn = document.querySelector('#btn');
-const contenedorCartas = document.querySelector('#contenedor-cards');
-const contendorCarrito = document.querySelector('#contendor-offcanvas')
-
-// FUNCIONES
-const crearCartas = (arrayCartas) => {
-
-    arrayCartas.forEach(habitacion => {
-
-        contenedorCartas.innerHTML += `<div class="card" style="width: 18rem;" id=${habitacion.id}>
-        <img src="${habitacion.imagen}" class="card-img-top" alt="${habitacion.nombre}">
-        <div class="card-body">
-            <h5 class="card-title">${habitacion.nombre}</h5>
-            <p class="card-text">${habitacion.descripcion}</p>
-            <button class="btn btn-primary" id=${habitacion.id}>Reservar</button>
-        </div>
-    </div>`
-
-    })
-}
-
-const crearCarta = (carta) => {
-
-    return (
-        `<div class="card" style="width: 18rem;">
-            <img src="${carta.imagen}" class="card-img-top" alt="${carta.nombre}">
-            <div class="card-body">
-                <h5 class="card-title">${carta.nombre}</h5>
-                <p class="card-text">${carta.descripcion}</p>
-                <a href="#" class="btn btn-primary" id="${carta.id}" >Reservar</a>
-            </div>
-        </div>`
-    )
-
-}
-
-const buscarYagregarProducto = (id) => {
-
-    const producto = habitaciones.find(habitacion => habitacion.id == id);
-
-    // console.log(crearCarta(producto));
-
-    carrito.push(producto);
-    console.log(carrito);
-    // console.log(producto);
-
-}
+const acciones = [
+    {
+        id: 1,
+        nombre: 'Agregar clientes',
+    },
+    {
+        id: 2,
+        nombre: 'Mostrar clientes'
+    },
+    {
+        id: 3,
+        nombre: 'Eliminar clientes'
+    }
+]
 
 
+// variable
+let clientes = [];
 
 
-// crearCartas(carta);
-crearCartas(habitaciones);
+// dom
+const formAgregarCliente = document.querySelector('#form-agregar-cliente');
+const formAcciones = document.querySelector('#form-acciones');
+const formEliminar = document.querySelector('#form-eliminar');
+const selectHabitacion = document.querySelector('#form-agregar-cliente select');
+const selectAcciones = document.querySelector('#form-acciones select');
+const selectEliminar = document.querySelector('#form-eliminar select');
+const tablaClientes = document.querySelector('#tabla-clientes');
 
-contenedorCartas.addEventListener('click', (e) => {
 
-    if (e.target.classList[0] === 'btn') {
+// funciones
+class Cliente {
 
+    constructor(nombre, nacimiento, habitacion, cantidadNoches, id) {
 
-        buscarYagregarProducto(Number(e.target.id));
-
+        this.nombre = nombre;
+        this.nacimiento = nacimiento;
+        this.habitacion = habitacion;
+        this.cantidadNoches = cantidadNoches;
+        this.id = id;
 
     }
+}
+/**
+ * 
+ * @returns {string} id generico
+ */
+const generarID = () => {
+    return Math.random().toString(30).substring(2);
+}
+/**
+ * 
+ * @param { select } nodo nodo del select donde se renderizaran las options
+ * @param { options } array array de opciones a renderizar
+ */
+const createOptions = (nodo, array) => {
+
+    nodo.innerHTML = '';
+
+    array.forEach(elementos => {
+
+        nodo.innerHTML += `<option value=${elementos.id}>${elementos.nombre}</option>`
+
+    })
+
+}
+
+/**
+ * 
+ * @param {number} id id de habitacion
+ * @returns habitacion que coincida con el id ingresado
+ */
+const buscarHabitacion = (id) => {
+
+    const habitacionElegida = habitaciones.find(habitacion => habitacion.id === Number(id))
+
+    return habitacionElegida;
+
+}
+
+/**
+ * Crea una tabla con los clientes que existen
+ */
+const createTable = () => {
+
+    document.querySelector('#tabla-clientes tbody').innerHTML = '';
+
+    clientes.forEach((cliente, index) => {
+
+        document.querySelector('#tabla-clientes tbody').innerHTML += `<tr>
+                    <th scope="row">${index + 1}</th>
+                    <td>${cliente.nombre}</td>
+                    <td>${cliente.habitacion.nombre}</td>
+                    <td>${cliente.cantidadNoches}</td>
+                </tr>`
+    })
+
+}
+
+/**
+ * Oculta todas las acciones disponibles con display none
+ */
+const ocultarTodos = () => {
+
+    formAgregarCliente.style.display = 'none';
+    tablaClientes.style.display = 'none';
+    formEliminar.style.display = 'none';
+
+}
+
+/**
+ * 
+ * @param {nodo} elemento elemento del cual se quiere cambiar el display
+ * @param {string} type atributo del style, block, none, inline, etc
+ */
+const cambiarEstilo = (elemento, type) => {
+
+    // elemento.setAttribute('style', `display: ${style};`);
+
+    elemento.style.display = type;
+
+}
+
+/**
+ * 
+ * @param {number} id recibe el id de la opcion seleccionada, muestra la opcion elegida y oculta las demas
+ */
+const redirigir = (id) => {
+
+    ocultarTodos();
+
+    switch (Number(id)) {
+
+        case 1:
+
+            cambiarEstilo(formAgregarCliente, 'block');
+
+            break;
+
+        case 2:
+
+            createTable();
+
+            cambiarEstilo(tablaClientes, 'table');
+
+            break;
+
+        case 3:
+
+            createOptions(selectEliminar, clientes);
+
+            cambiarEstilo(formEliminar, 'block');
+
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+/**
+ * Verifico en localStorage si existen clientes, si existen lo igualo a clientes
+ */
+const verificarStorage = () => {
+
+    console.log(clientes);
+
+    if (localStorage.getItem('clientes')) {
+
+        clientes = JSON.parse(localStorage.getItem('clientes'));
+
+        console.log(clientes);
+
+    }
+
+}
+
+/**
+ * 
+ * @param {array} clientes guardo en local storage el array clientes
+ */
+const setearClientesStorage = (clientes) => {
+
+    localStorage.setItem('clientes', JSON.stringify(clientes));
+
+}
+
+
+// code
+verificarStorage();
+createOptions(selectHabitacion, habitaciones);
+createOptions(selectAcciones, acciones);
+createOptions(selectEliminar, clientes);
+
+
+formAgregarCliente.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    clientes.push(new Cliente(e.target['nombre'].value, new Date(e.target['edad'].value), buscarHabitacion(e.target['habitacion'].value), Number(e.target['cantNoches'].value), generarID()));
+
+    setearClientesStorage(clientes);
+
+    formAgregarCliente.reset();
 })
 
+formAcciones.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    redirigir(e.target['accion'].value);
+
+})
+
+formEliminar.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    let clientesFiltrados = clientes.filter(cliente => cliente.id != e.target['eliminar-cliente'].value);
+
+    clientes = [...clientesFiltrados];
+
+    setearClientesStorage(clientes);
+
+    createOptions(selectEliminar, clientesFiltrados);
+})
 
